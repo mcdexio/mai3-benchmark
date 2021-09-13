a43b341425c1b867aef470cd060973523aff6915
ChampFu <champfu@mcdex.io>
2021-09-13 14:58:07 +0800

    add deployments log

---
+++ deployments/local_development.deployment.js
@@ -0,0 +1,74 @@
+{
+  "SymbolService": {
+    "type": "plain",
+    "name": "SymbolService",
+    "address": "0x27428D83f50370acCd5bb174018f9B74F24f6eC2",
+    "deployedAt": 77
+  },
+  "PoolCreator": {
+    "type": "plain",
+    "name": "PoolCreator",
+    "address": "0x8008b6ED29f17596fCBFafD327148CE6f6681496",
+    "deployedAt": 79
+  },
+  "AMMModule": {
+    "type": "plain",
+    "name": "AMMModule",
+    "address": "0xDc609f150Da9720b28c5E22687961Ae9725D1dE2",
+    "deployedAt": 82
+  },
+  "CollateralModule": {
+    "type": "plain",
+    "name": "CollateralModule",
+    "address": "0x1dDCa805A0A0e83cA3bf96707eDd409a096d60F9",
+    "deployedAt": 83
+  },
+  "PerpetualModule": {
+    "type": "plain",
+    "name": "PerpetualModule",
+    "address": "0x70c6cC739a18cCD560E9b642626E1E14121d7c42",
+    "deployedAt": 84
+  },
+  "LiquidityPoolModule": {
+    "type": "plain",
+    "name": "LiquidityPoolModule",
+    "address": "0xd8Cbae3ee493227B353Bfaa701238c06b4EF9404",
+    "deployedAt": 85
+  },
+  "OrderModule": {
+    "type": "plain",
+    "name": "OrderModule",
+    "address": "0x1B44256DE3d47Ea465f576d78B807f2b70C6f26B",
+    "deployedAt": 86
+  },
+  "TradeModule": {
+    "type": "plain",
+    "name": "TradeModule",
+    "address": "0xA39320836d6398F1390A5F73458f04254F076400",
+    "deployedAt": 87
+  },
+  "LiquidityPool": {
+    "type": "plain",
+    "name": "LiquidityPool",
+    "address": "0x8b7C68de128869F194dbbaBeE8c6d7BE922217b5",
+    "deployedAt": 88
+  },
+  "LpGovernor": {
+    "type": "plain",
+    "name": "LpGovernor",
+    "address": "0x615ecdccFe6aFb86352Bc4c8d19D17527Ad534B2",
+    "deployedAt": 89
+  },
+  "MockUSDC": {
+    "type": "plain",
+    "name": "MockUSDC",
+    "address": "0xebB0966DDa3b1C85D78AA6dbB84cdaf827C52bcA",
+    "deployedAt": 91
+  },
+  "OracleAdaptor": {
+    "type": "plain",
+    "name": "OracleAdaptor",
+    "address": "0x7e33d3c8ae8a6146D84c76C1d378e5eb0600B6FD",
+    "deployedAt": 93
+  }
+}
