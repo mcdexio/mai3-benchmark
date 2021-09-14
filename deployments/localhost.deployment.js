3f5d3bce4e797b4f48a39fa713152a2d62005c0e
ChampFu <champfu@mcdex.io>
2021-09-15 02:23:55 +0800

    done trade

--- deployments/localhost.deployment.js
+++ deployments/localhost.deployment.js
@@ -2,79 +2,85 @@
   "SymbolService": {
     "type": "plain",
     "name": "SymbolService",
-    "address": "0x0740BFEeAEb3c8a7b8718A4F3B20618568cDF621",
-    "deployedAt": 1350
+    "address": "0x2B8F5e69C35c1Aff4CCc71458CA26c2F313c3ed3",
+    "deployedAt": 906
   },
   "PoolCreator": {
     "type": "plain",
     "name": "PoolCreator",
-    "address": "0xE97166C46816d48B2aFFCfFf704B962E88fd0abE",
-    "deployedAt": 1352
+    "address": "0xA899118f4BCCb62F8c6A37887a4F450D8a4E92E0",
+    "deployedAt": 908
   },
   "AMMModule": {
     "type": "plain",
     "name": "AMMModule",
-    "address": "0x9A676e781A523b5d0C0e43731313A708CB607508",
-    "deployedAt": 15
+    "address": "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+    "deployedAt": 6
   },
   "CollateralModule": {
     "type": "plain",
     "name": "CollateralModule",
-    "address": "0x0B306BF915C4d645ff596e518fAf3F9669b97016",
-    "deployedAt": 16
+    "address": "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+    "deployedAt": 7
   },
   "PerpetualModule": {
     "type": "plain",
     "name": "PerpetualModule",
-    "address": "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1",
-    "deployedAt": 17
+    "address": "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+    "deployedAt": 8
   },
   "LiquidityPoolModule": {
     "type": "plain",
     "name": "LiquidityPoolModule",
-    "address": "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
-    "deployedAt": 18
+    "address": "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
+    "deployedAt": 9
   },
   "OrderModule": {
     "type": "plain",
     "name": "OrderModule",
-    "address": "0x68B1D87F95878fE05B998F19b66F4baba5De1aed",
-    "deployedAt": 19
+    "address": "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+    "deployedAt": 10
   },
   "TradeModule": {
     "type": "plain",
     "name": "TradeModule",
-    "address": "0x3Aa5ebB10DC797CAC828524e59A333d0A371443c",
-    "deployedAt": 20
+    "address": "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+    "deployedAt": 11
   },
   "LiquidityPool": {
     "type": "plain",
     "name": "LiquidityPool",
-    "address": "0x54c53FB74C17a2a1B28377C2C2b11D0351367eE0",
-    "deployedAt": 1355
+    "address": "0xF94AB55a20B32AC37c3A105f12dB535986697945",
+    "deployedAt": 911
   },
   "LpGovernor": {
     "type": "plain",
     "name": "LpGovernor",
-    "address": "0xa6Ca882f693f7c613cE1bAe28B91A06eaAcA8DD9",
-    "deployedAt": 1356
+    "address": "0xBCF063A9eB18bc3C6eB005791C61801B7cB16fe4",
+    "deployedAt": 912
   },
   "CustomERC20": {
     "type": "plain",
     "name": "CustomERC20",
-    "address": "0x1f871a3fe5956F642CfF1671eEe7a886E4986a2d",
-    "deployedAt": 1358
+    "address": "0x364C7188028348566E38D762f6095741c49f492B",
+    "deployedAt": 914
   },
   "OracleAdaptor": {
     "type": "plain",
     "name": "OracleAdaptor",
-    "address": "0x2B3323Dba63a4a1Ed0a4B02d0B3fD5C901760881",
-    "deployedAt": 1360
+    "address": "0xF2cb3cfA36Bfb95E0FD855C1b41Ab19c517FcDB9",
+    "deployedAt": 916
   },
   "Disperse": {
     "type": "plain",
     "name": "Disperse",
-    "address": "0x9090EcAeBa8d113e49ad8Ca83Bf9FB516C723885",
-    "deployedAt": 1367
+    "address": "0x8659DF1C638CDA8E475CD3C6481730C2b4f85873",
+    "deployedAt": 923
+  },
+  "Reader": {
+    "type": "plain",
+    "name": "Reader",
+    "address": "0x162700d1613DfEC978032A909DE02643bC55df1A",
+    "deployedAt": 256
   }
 }
--- scripts/benchmark.ts
+++ scripts/benchmark.ts
@@ -13,17 +13,16 @@ const ENV: DeploymentOptions = {
 
 function toWei(n) { return hre.ethers.utils.parseEther(n) }
 
-const vaultFeeRate = toWei("0");
+const vaultFeeRate = toWei("0.001");
 const vault = "0x81183C9C61bdf79DB7330BBcda47Be30c0a85064"
-const USE_TARGET_LEVERAGE = 0x8000000;
+const USE_TARGET_LEVERAGE = 0x08000000;
 const NONE = "0x0000000000000000000000000000000000000000";
 let masterAcc;
 let traders = [];
 let mockUSDCContract;
 let disperseContract;
 let latestLiquidityPoolContract;
-let traderModuleContract;
-let readerContract;
+let poolCreatorContract;
 
 async function distribute(count: number, ethers) {
   const startTime = Date.now()
@@ -35,22 +34,23 @@ async function distribute(count: number, ethers) {
     traders.push(newWallet)
     values.push(1)
   }
-  // ETH: distribute 1 eth to count of accounts
+  // ETH: distribute 1 eth to traders
   await disperseContract.connect(masterAcc).disperseEther(
     traders.map(X => X.address),
-    traders.map(X => toWei("1")),
-    {value: toWei("10")}
+    traders.map(X => toWei("1")), // 1 eth
+    {value: toWei(count.toString())}
   )
+  // USDC: distribute 100 usdc to traders
   await disperseContract.connect(masterAcc).disperseToken(
     mockUSDCContract.address,
     traders.map(X => X.address),
-    traders.map(X => 100000000)
+    traders.map(X => toWei("10000"))
   )
   const endTime = Date.now()
   console.log("Done distribute end " + endTime + " spend time " + (endTime-startTime)/1000)
   const tx = async (trader) => {
-    await mockUSDCContract.connect(trader).approve(latestLiquidityPoolContract.address, "100" + "000000")
-    await latestLiquidityPoolContract.connect(trader).setTargetLeverage(0, trader.address, toWei("25"))
+    await mockUSDCContract.connect(trader).approve(latestLiquidityPoolContract.address, toWei("100000"))
+    await latestLiquidityPoolContract.connect(trader).setTargetLeverage(0, trader.address, toWei("10"))
   }
   const txs = traders.map(x => tx(x))
   await Promise.all(txs)
@@ -67,82 +67,88 @@ async function setup(ethers, deployer, accounts) {
 
   // PoolCreator
   await deployer.deploy("PoolCreator")
-  const poolCreator = await deployer.getDeployedContract("PoolCreator")
+  poolCreatorContract = await deployer.getDeployedContract("PoolCreator")
   console.log("Done deploy PoolCreator")
-  await ensureFinished(poolCreator.initialize(
+  await ensureFinished(poolCreatorContract.initialize(
     deployer.addressOf("SymbolService"),
     vault,
     vaultFeeRate
   ))
-  await ensureFinished(symbolService.addWhitelistedFactory(poolCreator.address))
+  await ensureFinished(symbolService.addWhitelistedFactory(poolCreatorContract.address))
 
   // LiquidityPool
   const liquidityPool = await deployer.deploy("LiquidityPool")
   const governor = await deployer.deploy("LpGovernor")
   console.log("Done deploy liquidityPool, governor")
-  await ensureFinished(poolCreator.addVersion(liquidityPool.address, governor.address, 0, "initial version"))
+  await ensureFinished(poolCreatorContract.addVersion(liquidityPool.address, governor.address, 0, "initial version"))
 
   // deploy mock erc20
-  mockUSDCContract = await deployer.deploy("CustomERC20", "MockUSDC", "MockUSDC", 6)
+  mockUSDCContract = await deployer.deploy("CustomERC20", "MockUSDC", "MockUSDC", 18)
   console.log("Done deploy mockUSDCContract")
-  await ensureFinished(poolCreator.createLiquidityPool(
+  await ensureFinished(poolCreatorContract.createLiquidityPool(
     mockUSDCContract.address,
-    6,
+    18,
     Math.floor(Date.now() / 1000),
-    ethers.utils.defaultAbiCoder.encode(["bool", "int256"], [false, toWei("10000000")])
+    ethers.utils.defaultAbiCoder.encode(["bool", "int256"], [false, toWei("1000000")])
   ))
 
   // deploy oracle
   const oracle = await deployer.deploy("OracleAdaptor", "USD", "ETH")
   console.log("Done deploy oracleAdaptor")
   let now = Math.floor(Date.now() / 1000);
-  await ensureFinished(oracle.setIndexPrice(toWei("100"), now))
-  await ensureFinished(oracle.setMarkPrice(toWei("100"), now))
+  await ensureFinished(oracle.setIndexPrice(toWei("500"), now))
+  await ensureFinished(oracle.setMarkPrice(toWei("500"), now))
 
   // createPerpetual
-  const n = await poolCreator.getLiquidityPoolCount();
-  const allLiquidityPools = await poolCreator.listLiquidityPools(0, n.toString());
+  const n = await poolCreatorContract.getLiquidityPoolCount();
+  const allLiquidityPools = await poolCreatorContract.listLiquidityPools(0, n.toString());
   latestLiquidityPoolContract = await deployer.getContractAt("LiquidityPool", allLiquidityPools[allLiquidityPools.length - 1]);
+  console.log("LiquidityPool " + latestLiquidityPoolContract.address)
   await ensureFinished(latestLiquidityPoolContract.createPerpetual(
     oracle.address,
-    // imr          mmr            operatorfr        lpfr              rebate        penalty        keeper       insur         oi
-    [toWei("0.04"), toWei("0.03"), toWei("0.00010"), toWei("0.00055"), toWei("0.2"), toWei("0.01"), toWei("10"), toWei("0.5"), toWei("3")],
-    // alpha           beta1            beta2             frLimit        lev         maxClose       frFactor        defaultLev
-    [toWei("0.00075"), toWei("0.0075"), toWei("0.00525"), toWei("0.01"), toWei("1"), toWei("0.05"), toWei("0.005"), toWei("10")],
-    [toWei("0"),       toWei("0"),      toWei("0"),       toWei("0"),    toWei("0"), toWei("0"),    toWei("0"),     toWei("0")],
-    [toWei("0.1"),     toWei("0.5"),    toWei("0.5"),     toWei("0.1"),  toWei("5"), toWei("1"),    toWei("0.1"),   toWei("10000000")]
+    [toWei("0.1"), toWei("0.05"), toWei("0.001"), toWei("0.001"), toWei("0.2"), toWei("0.02"), toWei("0"), toWei("0.5"), toWei("5")],
+    [toWei("0.01"), toWei("0.1"), toWei("0.06"), toWei("0"), toWei("5"), toWei("0.05"), toWei("0.01"), toWei("0")],
+    [toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0")],
+    [toWei("0.1"), toWei("0.2"), toWei("0.2"), toWei("0.5"), toWei("10"), toWei("0.99"), toWei("1"), toWei("1")]
   ))
   await ensureFinished(latestLiquidityPoolContract.runLiquidityPool())
   console.log("Done create perpetual")
-  await ensureFinished(mockUSDCContract.mint(masterAcc.address, "10000" + "000000"))
-  await ensureFinished(mockUSDCContract.connect(masterAcc).approve(latestLiquidityPoolContract.address, "10000" + "000000"))
+  await ensureFinished(mockUSDCContract.mint(masterAcc.address, toWei("10000000")))
+  await ensureFinished(mockUSDCContract.connect(masterAcc).approve(latestLiquidityPoolContract.address, toWei("10000000")))
 
   // disperse (
   await deployer.deploy("Disperse")
   disperseContract = await deployer.getDeployedContract("Disperse")
-  await ensureFinished(mockUSDCContract.connect(masterAcc).approve(disperseContract.address, "10000" + "000000"))
+  await ensureFinished(mockUSDCContract.connect(masterAcc).approve(disperseContract.address, toWei("10000000")))
   console.log("Done deploy disperseContract")
-  await distribute(10,ethers)
+  await distribute(1,ethers)
 
-  await ensureFinished(latestLiquidityPoolContract.connect(masterAcc).addLiquidity("1000"+"000000"))
+  await ensureFinished(latestLiquidityPoolContract.connect(masterAcc).addLiquidity("1000"))
   console.log("Done add liquidity")
 }
 
-async function benchmark() {
+async function benchmark(deployer) {
+  console.log("trader0 add " + traders[0].address)
+  // await deployer.deploy("Reader", poolCreatorContract.address)
+  // const reader = await deployer.getDeployedContract("Reader")
+  // const info = await reader.callStatic.getMarginAccount(0, traders[0].address)
+  // console.log(info)
   const startTime = Date.now()
   console.log("start trader " + startTime)
-  const txs = traders.map(x => latestLiquidityPoolContract.connect(x).trade(0, x.address, toWei("1"), toWei("100"), NONE, USE_TARGET_LEVERAGE))
-  await Promise.all(txs)
+  // const txs = traders.map(x => latestLiquidityPoolContract.connect(x).deposit(0, x.address, toWei("600")))
+  // await Promise.all(txs)
+  let now = Math.floor(Date.now() / 1000);
+  const txs1 = traders.map(x => latestLiquidityPoolContract.connect(x).trade(0, x.address, 1, toWei("600"), now + 999999, NONE, USE_TARGET_LEVERAGE))
+  await Promise.all(txs1)
   const end1Time = Date.now()
-  console.log("Done trade end " + end1Time + " spend time " + (end1Time-startTime)/1000)
-  await Promise.all(txs.map(x => x.wait()))
+  console.log("Done trade end1 " + end1Time + " spend time " + (end1Time-startTime)/1000)
   const end2Time = Date.now()
-  console.log("Done trade end " + end2Time + " spend time " + (end2Time-startTime)/1000)
+  console.log("Done trade end2 " + end2Time + " spend time " + (end2Time-startTime)/1000)
 }
 
 async function main(ethers, deployer, accounts) {
   await setup(ethers, deployer, accounts)
-  await benchmark()
+  await benchmark(deployer)
 }
 
 ethers.getSigners()
