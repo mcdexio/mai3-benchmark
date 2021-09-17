e32ea528322b932de2372353371ce7f8ce1b92d5
ChampFu <champfu@mcdex.io>
2021-09-17 11:27:16 +0800

    done remove-whitelist

--- benchmark.sh
+++ benchmark.sh
@@ -16,6 +16,16 @@ yarn
 yarn build
 
 cd $GITROOT
-# build geth
+# Build L1: equal to docker:build:geth, docker:geth
 docker-compose up arb-bridge-eth-geth
+while ! nc -z localhost 7545; do sleep 2; done;
+echo "Finished waiting for geth on localhost:7545..."
 yarn deploy:live --network local_development --export bridge_eth_addresses.json && [ -f bridge_eth_addresses.json ]
+echo "Deploy Arbitrum(L2) contract to L1"
+
+# Build L2: equal to yarn demo:initialize, demo:deploy
+yarn demo:initialize
+yarn demo:deploy
+
+# Setup L2
+yarn remove-whitelist
