fc3229d9607f6e3382ee09f51143c4be32b264d1
ChampFu <champfu@mcdex.io>
2021-09-17 14:13:22 +0800

    align docker-compose for Arbitrum

--- benchmark.sh
+++ benchmark.sh
@@ -25,7 +25,7 @@ echo "Deploy Arbitrum(L2) contract to L1"
 
 # Build L2: equal to yarn demo:initialize, demo:deploy
 yarn demo:initialize
-yarn demo:deploy --detach
+yarn demo:deploy --detach --benchmark-docker-compose-path=$GITROOT/docker-compose.yml
 
 # Setup L2
 yarn remove-whitelist
